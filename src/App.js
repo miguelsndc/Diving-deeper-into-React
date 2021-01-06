ffea937f611b710ccd89b02c5edc4e054095d5bc
miguelsndc <67760107+miguelsndc@users.noreply.github.com>
2021-01-06 15:27:46 -0300

    Changing Folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-// import Setup from './content/1-useState/setup/1-error-example'
-// import Setup from './content/1-useState/setup/2-useState-basics'
-import Setup from './content/1-useState/setup/3-useState-array'
-// import Setup from './content/1-useState/setup/4-useState-object'
-// import Setup from './content/1-useState/setup/5-useState-counter'
+// import Setup from './content/1-useState/1-error-example'
+// import Setup from './content/1-useState/2-useState-basics'
+// import Setup from './content/1-useState/3-useState-array'
+// import Setup from './content/1-useState/4-useState-object'
+// import Setup from './content/1-useState/5-useState-counter'
+
+import Setup from './content/2-useEffect/1-useEffect-basics'
 
 function App() {
   return (
