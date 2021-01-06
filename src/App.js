8e9bfd3f5923c0b6fa9a04868307887abe4eaf29
miguelsndc <67760107+miguelsndc@users.noreply.github.com>
2021-01-06 18:08:18 -0300

    useEffect & Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import React from 'react'
 // import Setup from './content/1-useState/4-useState-object'
 // import Setup from './content/1-useState/5-useState-counter'
 
-import Setup from './content/2-useEffect/1-useEffect-basics'
+// import Setup from './content/2-useEffect/1-useEffect-basics'
+// import Setup from './content/2-useEffect/2-useEffect-cleanup'
+// import Setup from './content/2-useEffect/3-useEffect-fetch-data'
+import Setup from './content/3-conditional-rendering/1-multiple-returns'
 
 function App() {
   return (
     <div className='container'>
-      {/* <Setup /> */}
       <Setup />
     </div>
   )
--- src/content/3-conditional-rendering/1-multiple-returns.js
+++ src/content/3-conditional-rendering/1-multiple-returns.js
@@ -1,7 +1,48 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
-const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+import React, { useState, useEffect } from 'react'
 
-export default MultipleReturns;
+const url = 'https://api.github.com/users/QuincyLarson'
+
+const MultipleReturns = () => {
+  const [isLoading, setisLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('Default user')
+
+  const getUserData = () => {
+    // this func code is a mess, but it is for learning purposes, i might refactor it someday, pls dont judge
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json()
+        } else {
+          setisLoading(false)
+          setIsError(true)
+          throw new Error(res.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user
+        setUser(login)
+        setisLoading(false)
+      })
+      .catch((e) => {
+        setIsError(true)
+        console.log(e)
+      })
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (isError) {
+    return <h2>Error</h2>
+  }
+
+  return <h2>{user}</h2>
+}
+
+export default MultipleReturns
